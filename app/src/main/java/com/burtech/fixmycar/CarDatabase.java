48d86b81d106535b2795308bfb239e3ad420e0c1
ryancburton <ryancburton@yahoo.co.uk>
2020-10-25 01:15:48 +0100

    latest

---
+++ app/src/main/java/com/burtech/fixmycar/CarDatabase.java
@@ -0,0 +1,118 @@
+package com.burtech.fixmycar;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class CarDatabase extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "myCars.db";
+    private static final String TABLE_CarParts = "CarParts";
+
+    private static final String FIELD_CAR_PART = "CARPART";
+    private static final String FIELD_PRICE = "PRICE";
+    private static final String FIELD_DURATION = "DURATION";
+    private static final int DATABASE_VERSION = 1;
+
+    CarDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_CarParts + "(_id integer PRIMARY KEY," + FIELD_CAR_PART + " TEXT, " + FIELD_PRICE + " INTEGER, " + FIELD_DURATION + " INTEGER );");
+        Log.i("onCreate", "onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Handle database upgrade as needed
+    }
+
+    public boolean saveRecord(String carPart, Integer carPrice, Integer Duration) {
+        try
+        {
+            long id = findCarPartID(carPart);
+            if (id>0)
+            {
+                updateRecord(id, carPart, carPrice, Duration);
+                return false;
+            }
+            else
+            {
+                addRecord(carPart, carPrice, Duration);
+                return true;
+            }
+        }
+        catch (SQLException ex)
+        {
+            Log.i("findCarPartID", ex.getMessage());
+            return false;
+        }
+    }
+
+    public long addRecord(String carPart, Integer carPrice, Integer Duration) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(FIELD_CAR_PART, carPart);
+        values.put(FIELD_PRICE, carPrice);
+        values.put(FIELD_DURATION, Duration);
+        return db.insert(TABLE_CarParts, null, values);
+    }
+
+    public int updateRecord(long id, String carPart, Integer carPrice, Integer Duration) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("_id", id);
+        values.put(FIELD_CAR_PART, carPart);
+        values.put(FIELD_PRICE, carPrice);
+        values.put(FIELD_DURATION, Duration);
+        return db.update(TABLE_CarParts, values, "_id = ?", new String[]{String.valueOf(id)});
+    }
+
+    public int deleteRecord(long id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(TABLE_CarParts, "_id = ?", new String[]{String.valueOf(id)});
+    }
+
+    public long findCarPartID(String carPart) {
+        long returnVal = -1;
+        SQLiteDatabase db = getReadableDatabase();
+        try
+        {
+            String query = "SELECT _id FROM " + TABLE_CarParts + " WHERE " + FIELD_CAR_PART + " = " + "\'" +  carPart + "\'";
+            Cursor cursor = db.rawQuery(query, null);
+            Log.i("findCarPartID", "getCount()=" + cursor.getCount());
+
+            if (cursor.getCount() > 0)
+            {
+                cursor.moveToFirst();
+                returnVal = cursor.getInt(0);
+            }
+            return returnVal;
+        }
+        catch (SQLException ex)
+        {
+            Log.i("findCarPartID", ex.getMessage());
+            return -1;
+        }
+    }
+
+    public Cursor getCarPartsList() {
+        SQLiteDatabase db = getReadableDatabase();
+        try
+        {
+            String query = "SELECT _id, " + FIELD_CAR_PART + ", " + FIELD_PRICE + ", "  + FIELD_DURATION  + " FROM " + TABLE_CarParts + " ORDER BY " + FIELD_CAR_PART + " ASC";;
+            return db.rawQuery(query, null);
+        }
+        catch (SQLException ex)
+        {
+            Log.i("CarDatabase", ex.getMessage());
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/burtech/fixmycar/MainActivity.java
@@ -0,0 +1,209 @@
+package com.burtech.fixmycar;
+
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditCarPart;
+    EditText mEditPrice;
+    EditText mEditDuration;
+
+    CarDatabase mDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDB = new CarDatabase(this);
+
+        try {
+            // Create URL
+            URL githubEndpoint = new URL("https://api.github.com/");
+            // Create connection
+            HttpsURLConnection myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
+        }
+        catch(Exception c) {
+        }
+
+        mEditCarPart = findViewById(R.id.CarPart);
+        mEditPrice = findViewById(R.id.Price);
+        mEditDuration = findViewById(R.id.Duration);
+
+        Button buttonAddUpdate = findViewById(R.id.AddCarPart);
+
+        if(buttonAddUpdate != null)
+        {
+            buttonAddUpdate.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+
+                    if(mEditCarPart.getText().toString().equalsIgnoreCase(""))
+                    {
+                        Toast errorToast = Toast.makeText(MainActivity.this, "Please enter Valid Car Part.", Toast.LENGTH_SHORT);
+                        errorToast.show();
+                        return;
+                    }
+
+                    if(!tryParseInt(mEditPrice.getText().toString()))
+                    {
+                        Toast errorToast = Toast.makeText(MainActivity.this, "Please enter Price.", Toast.LENGTH_SHORT);
+                        errorToast.show();
+                        return;
+                    }
+
+                    if(!tryParseInt(mEditDuration.getText().toString()))
+                    {
+                        Toast errorToast = Toast.makeText(MainActivity.this, "Please enter Duruation.", Toast.LENGTH_SHORT);
+                        errorToast.show();
+                        return;
+                    }
+
+                    if (mDB.saveRecord(mEditCarPart.getText().toString(), Integer.parseInt(mEditPrice.getText().toString()), Integer.parseInt(mEditDuration.getText().toString())))
+                    {
+                        addNewCarPart();
+                    }
+                    /*else
+                    {
+                        updateCarPartTable();
+                    }*/
+                }
+            });
+        }
+
+        //addHeadings();
+        updateCarPartTable();
+    }
+
+    boolean tryParseInt(String value)
+    {
+        try
+        {
+            Integer.parseInt(value);
+            return true;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }
+
+    public void addHeadings()
+    {
+        TableLayout carParts = findViewById(R.id.carPartsTable);
+        //carParts.removeAllViews();
+
+        TableRow tableRow = findViewById(R.id.carPartsTableRow);
+        TextView colCarPart = findViewById(R.id.colCarPart);
+        TextView colPrice = findViewById(R.id.colPrice);
+        TextView colDuration = findViewById(R.id.colDuration);
+
+        tableRow.addView(colCarPart);
+        tableRow.addView(colPrice);
+        tableRow.addView(colDuration);
+        carParts.addView(tableRow);
+    }
+
+    public void Add_Headings()
+    {
+        TableLayout carParts = (TableLayout)findViewById(R.id.carPartsTable);
+        //carParts.removeAllViews();
+        carParts.setStretchAllColumns(true);
+        carParts.bringToFront();
+
+        TableRow tr = new TableRow(this);//(TableRow) findViewById(R.id.tableRow);
+        TextView c1 = new TextView(this);
+        c1.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        //c1.setWidth(150);
+        c1.setText("Car Part");
+        TextView c2 = new TextView(this);
+        c2.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        c2.setText("Price");
+        //c1.setWidth(150);
+        TextView c3 = new TextView(this);//(TextView) findViewById(R.id.col3);
+        c3.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        c3.setText("Duration");
+        //c1.setWidth(150);
+        tr.addView(c1);
+        tr.addView(c2);
+        tr.addView(c3);
+        carParts.addView(tr);
+    }
+
+    public void addNewCarPart()
+    {
+        TableLayout carParts = findViewById(R.id.carPartsTable);
+        //carParts.removeAllViews();
+        carParts.setStretchAllColumns(true);
+        carParts.bringToFront();
+
+        //TableRow tr = findViewById(R.id.carPartsTableRow);
+
+        TableRow tr = new TableRow(this);
+
+        TextView c1 = new TextView(this);
+        c1.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        //TableRow.LayoutParams params = new TableRow.LayoutParams(2, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+        //c1.setLayoutParams(params);
+        c1.setText(mEditCarPart.getText());
+        TextView c2 = new TextView(this);
+        c2.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        c2.setText(mEditPrice.getText());
+        //params = new TableRow.LayoutParams(1, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+        //c2.setLayoutParams(params);
+        TextView c3 = new TextView(this);//(TextView) findViewById(R.id.col3);
+        c3.setBackgroundColor(Color.parseColor("#D8FFB9"));
+        c3.setText(mEditDuration.getText());
+        //params = new TableRow.LayoutParams(1, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+        //c3.setLayoutParams(params);
+
+        tr.addView(c1);
+        tr.addView(c2);
+        tr.addView(c3);
+        carParts.addView(tr);
+    }
+
+    public void updateCarPartTable()
+    {
+        Cursor cursor = mDB.getCarPartsList();
+
+        if(cursor == null)
+            return;
+
+        TableLayout carParts = findViewById(R.id.carPartsTable);
+        //carParts.removeAllViews();
+        carParts.setStretchAllColumns(true);
+        carParts.bringToFront();
+
+        while(cursor.moveToNext())
+        {
+            TableRow tr =  new TableRow(this);//(TableRow) findViewById(R.id.tableRow);
+            TextView c1 = new TextView(this);
+           // c1.setBackgroundColor(Color.parseColor("#D8FFB9"));
+            c1.setText(cursor.getString(1));
+            TextView c2 = new TextView(this);
+            //c1.setBackgroundColor(Color.parseColor("#D8FFB9"));
+            c2.setText(cursor.getString(2));
+            TextView c3 = new TextView(this);//(TextView) findViewById(R.id.col3);
+            //c3.setBackgroundColor(Color.parseColor("#D8FFB9"));
+            c3.setText(cursor.getString(3));
+            tr.addView(c1);
+            tr.addView(c2);
+            tr.addView(c3);
+            carParts.addView(tr);
+        }
+    }
+}
